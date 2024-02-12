ebb97be883353ec86eec67b09ec90f700220c27a
Java <ao.alina@gmail.com>
2024-02-12 21:26:09 +0200

    hhh1

--- src/main/java/PriceChecker.java
+++ src/main/java/PriceChecker.java
@@ -1,12 +1,20 @@
-public class Task3 {
+public class PriceChecker {
     public static void main(String[] args) {
-        boolean isValidPrice = checkPrice(4, 3);
+        boolean isValidPrice = checkPrice(2, 3);
+        boolean isValidPrice2 = checkPrice(10, 3);
 
-        if (isValidPrice) {
+        /*if (isValidPrice) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }*/
+
+        if (!(isValidPrice || !isValidPrice2)) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
+
     }
     private static boolean checkPrice(int number1, int number2) {
         if (number1 > number2) {
